f48e35058958355612c2975f35172d8c043797b2
Juan David Sánchez <juancho85@gmail.com>
2017-01-23 13:13:11 +0100

    unfavorite feature implemented. Prevent adding multiple times the same quote to favorites

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -20,7 +20,7 @@ export class QuotesPage implements OnInit {
     this.quoteGroup = this.navParams.data;
   }
 
-  onAddToFavorite(selectedQuote: Quote){
+  onAddToFavorites(selectedQuote: Quote){
     const alert = this.alertCtrl.create({
       title: 'Add Quote',
       subTitle: 'Are you sure?',
@@ -43,6 +43,14 @@ export class QuotesPage implements OnInit {
     });
 
     alert.present();
+  }
+
+  onRemoveFromFavorites(quote: Quote){
+    this.quotesService.removeQuoteFromFavorites(quote);
+  }
+
+  isFavorite(quote: Quote) {
+    return this.quotesService.isQuoteFavorite(quote);
   }
 
 
