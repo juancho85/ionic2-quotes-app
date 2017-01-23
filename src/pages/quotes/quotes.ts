d6d5071471c9cfe9e32234f8dc7a20aafed043bb
Juan David Sánchez <juancho85@gmail.com>
2017-01-23 12:17:53 +0100

    implement services for passing data around the pages

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {NavParams, AlertController} from "ionic-angular";
 import {Quote} from "../../data/quote.interface";
+import {QuotesService} from "../../services/quotes";
 
 @Component({
   selector: 'page-quotes',
@@ -12,7 +13,8 @@ export class QuotesPage implements OnInit {
 
   constructor(
     private navParams: NavParams,
-    private alertCtrl: AlertController){}
+    private alertCtrl: AlertController,
+    private quotesService: QuotesService){}
 
   ngOnInit(): void {
     this.quoteGroup = this.navParams.data;
@@ -27,7 +29,7 @@ export class QuotesPage implements OnInit {
         {
           text: 'Yes, go ahead!',
           handler: () => {
-            console.log("yes clicked");
+            this.quotesService.addQuoteToFavorites(selectedQuote);
           }
         },
         {
