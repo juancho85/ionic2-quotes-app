125c128cc502dcaf145d7196d92ee783e6799f73
Juan David Sánchez <juancho85@gmail.com>
2017-01-23 12:25:24 +0100

    retrieve the favorite quotes from the service and display them in a list

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -1,22 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import {Quote} from "../../data/quote.interface";
+import {QuotesService} from "../../services/quotes";
 
-/*
-  Generated class for the Favorites page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-favorites',
   templateUrl: 'favorites.html'
 })
 export class FavoritesPage {
+  quotes: Quote[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(private quotesService: QuotesService) {}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad FavoritesPage');
+  ionViewWillEnter() {
+    this.quotes = this.quotesService.getFavoriteQuotes();
   }
 
 }
