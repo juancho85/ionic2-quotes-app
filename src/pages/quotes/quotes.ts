bf5dae4a2345500a175bd991e3997d72437b5d52
Juan David Sánchez <juancho85@gmail.com>
2017-01-23 12:05:39 +0100

    add alerts and use it's API to define the fields and handlers

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {NavParams} from "ionic-angular";
+import {NavParams, AlertController} from "ionic-angular";
 import {Quote} from "../../data/quote.interface";
 
 @Component({
@@ -10,14 +10,37 @@ export class QuotesPage implements OnInit {
 
   quoteGroup: {category: string, quotes: Quote[], icon: string};
 
-  constructor(private navParams: NavParams){}
+  constructor(
+    private navParams: NavParams,
+    private alertCtrl: AlertController){}
 
   ngOnInit(): void {
     this.quoteGroup = this.navParams.data;
   }
 
-  onAddToFavorite(quote: Quote){
-    console.log("adding to favorite");
+  onAddToFavorite(selectedQuote: Quote){
+    const alert = this.alertCtrl.create({
+      title: 'Add Quote',
+      subTitle: 'Are you sure?',
+      message: 'Are you sure you want to add the quote?',
+      buttons: [
+        {
+          text: 'Yes, go ahead!',
+          handler: () => {
+            console.log("yes clicked");
+          }
+        },
+        {
+          text: 'No, not ready!',
+          role: 'cancel',//the handler is executed even if we dismiss by clicking outside
+          handler: () => {
+            console.log("Cancel!");
+          }
+        }
+      ]
+    });
+
+    alert.present();
   }
 
 
