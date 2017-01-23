6c5b4ca54b3132afa359a241ba44b5e727bae5b8
Juan David Sánchez <juancho85@gmail.com>
2017-01-23 11:35:19 +0100

    use cards to implement the quote details

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -4,7 +4,7 @@ import {LibraryPage} from "../library/library";
 @Component({
   selector: 'page-tabs',
   template: `
-    <ion-tabs selectedIndex="0">
+    <ion-tabs selectedIndex="1">
       <ion-tab [root]="favoritesPage" tabTitle="Favorites" tabIcon="star"></ion-tab>
       <ion-tab [root]="libraryPage" tabTitle="Library" tabIcon="book"></ion-tab>
     </ion-tabs>
